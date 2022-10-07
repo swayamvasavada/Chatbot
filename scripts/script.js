4b4a21a9a6beae3e95f0570bb936f51a88f1f5bc
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2022-10-07 16:42:29 +0530

    First version of Site is added

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const chatbot_logo = document.getElementById('chatbot-img');
+const chatbot = document.getElementById('chatbot');
+
+function chatbot_Open() {
+    chatbot.style.display = 'block';
+    chatbot_logo.style.display = 'none';
+}
+
+chatbot_logo.addEventListener('click', chatbot_Open);
