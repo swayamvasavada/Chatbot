f67f5dbd53193ac078955c260f580ed93f50929c
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2022-10-07 21:51:12 +0530

    Added more Design

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 const chatbot_logo = document.getElementById('chatbot-img');
 const chatbot = document.getElementById('chatbot');
+ 
+
 
 function chatbot_Open() {
     chatbot.style.display = 'block';
     chatbot_logo.style.display = 'none';
 }
+
 
 chatbot_logo.addEventListener('click', chatbot_Open);
